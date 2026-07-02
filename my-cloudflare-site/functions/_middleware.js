36081c4d9bc9774cde2461d2c0258870955652ab
ywd2279406651 <yewendu2@gmail.com>
2026-07-02 14:00:27 +0800

    Delete my-cloudflare-site directory

--- my-cloudflare-site/functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-// 全局中间件，拦截所有 /api/* 请求
-export async function onRequest(context) {
-  const { next, request } = context
-  console.log('全局中间件触发，请求路径：', new URL(request.url).pathname)
-
-  // 放行请求，执行后续接口逻辑
-  return next()
-}
