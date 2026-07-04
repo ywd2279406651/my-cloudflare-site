e7687df7a249545353d0d2f0f3af867bf4400710
ywd2279406651 <yewendu2@gmail.com>
2026-07-04 17:02:43 +0800

    更新 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+const buildTime = new Date().getTime()
 
 export default defineConfig({
-  // 根目录：如果你的 html 都在 src 下就保留这行，在根目录则删掉
   root: 'src',
-  
-  // 构建配置：指定所有页面入口
   build: {
     outDir: '../dist',
+    manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
         links: resolve(__dirname, 'src/links.html'),
         gallery: resolve(__dirname, 'src/gallery.html'),
         about: resolve(__dirname, 'src/about.html'),
-        subscribe: resolve(__dirname, 'src/subscribe.html') // 新增订阅页入口
+        subscribe: resolve(__dirname, 'src/subscribe.html')
+      },
+      output: {
+        entryFileNames: `assets/js/[name]-[hash]-${buildTime}.js`,
+        chunkFileNames: `assets/js/[name]-[hash]-${buildTime}.js`,
+        assetFileNames: `assets/[ext]/[name]-[hash]-${buildTime}.[ext]`
       }
     }
   },
-  
-  // 本地开发服务器配置
   server: {
     port: 5173,
     open: '/index.html'
